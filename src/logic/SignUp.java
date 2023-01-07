74a27aae22ba0ecbb99d961576c2464a93b5131d
Eman <emannasef21@gmail.com>
2023-01-07 18:36:32 +0200

    do some changes in signUp implementation

--- src/logic/SignUp.java
+++ src/logic/SignUp.java
@@ -22,7 +22,7 @@ public class SignUp {
         signUpButton();
     }
 
-    public String[] signUpTextFields() {
+    public String signUpTextFields() {
         userName = signUpScreenBase.userNameTextField.getText();
         password = signUpScreenBase.passwordTextField.getText();
         confirmPassword = signUpScreenBase.confirmTextField.getText();
@@ -31,7 +31,7 @@ public class SignUp {
         boolean isValiad = signUpScreenBase.validEmail(email);
         
         if (isValiad || confirmPassword.equals(password) || userName != null || gender=="Not Selected") {
-            String[] data = {userName, password, confirmPassword, email, gender};
+            String data ="SignUp"+ userName+","+ password +","+ confirmPassword+","+ email+","+ gender;
             return data;
         }
         
@@ -45,10 +45,11 @@ public class SignUp {
             public void handle(ActionEvent event) {
                 clientSide.ps.println("I clicked signup");
                 if (signUpTextFields() != null) {
-                    String[] signUpData = signUpTextFields();
+                    clientSide.ps.println(signUpTextFields());
+                   /* String[] signUpData = signUpTextFields();
                     for (int i = 0; i < signUpData.length; i++) {
                         clientSide.ps.println(signUpData[i]);
-                    }
+                    }*/
                 }
             }
         });
